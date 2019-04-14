820dac95387ab2ea04e94364d5034e670d71fcb7
Sanjeev Tripathi <sanjeevmail309@gmail.com>
2019-04-15 01:03:34 +0530

    feat(preferences): Added global preference class to set, get and manage settings, state and stats

--- core/src/com/studio/sanjeev/storklight/utility/Prefs.java
+++ core/src/com/studio/sanjeev/storklight/utility/Prefs.java
@@ -1,4 +1,27 @@
 package com.studio.sanjeev.storklight.utility;
 
-public class Preferences {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
+
+public class Prefs {
+
+    private Preferences pref ;
+    private boolean hasSound;
+
+    public Prefs(){
+        pref = Gdx.app.getPreferences("Storklight");
+        hasSound = pref.getBoolean("hasSound",true);
+    }
+
+    public void setSound(boolean hasSound){
+        this.hasSound=hasSound;
+        pref.putBoolean("hasSound",hasSound);
+        pref.flush();
+    }
+
+    public boolean getSound(){
+        return hasSound;
+    }
+
+
 }
