5be0dd5891491b859688eaa2fffd17aaf3a76556
Sanjeev Tripathi <sanjeevmail309@gmail.com>
2019-04-15 00:59:46 +0530

    feat(menu): Added menu to enable selection based on touch coordinates. Status: Only play button is functional, TBD

--- core/src/com/studio/sanjeev/storklight/states/MenuState.java
+++ core/src/com/studio/sanjeev/storklight/states/MenuState.java
@@ -5,9 +5,10 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import com.studio.sanjeev.storklight.StorkLightGameClass;
+import com.studio.sanjeev.storklight.utility.Prefs;
 
 
 /**
@@ -17,60 +18,83 @@ import com.studio.sanjeev.storklight.StorkLightGameClass;
 public class MenuState extends State {
     private Texture background;
     private Texture playStork;
-    private Texture playBtn;
     private BitmapFont font;
+    private Prefs prefs;
+
+    private static Boolean PAUSED = false;
 
 
     public MenuState(GameStateManager gsm, OrthographicCamera cam, Viewport viewport, Stage stage) {
         super(gsm,cam, viewport, stage);
 
-        font = new BitmapFont(Gdx.files.internal("fonts/abel.fnt"),Gdx.files.internal("fonts/abel.png"),false);
-        font.getData().setScale(0.2f);
 
-        background = new Texture("n0.png");
+        prefs = new Prefs();
+        font = new BitmapFont(Gdx.files.internal("fonts/abel.fnt"), Gdx.files.internal("fonts/abel.png"), false);
+        font.getData().setScale(0.15f);
+//        font.getData().setColor(1.0f,1.0f,1.0f,0.0f);
+        background = new Texture("artwork/main_landing_screen.png");
+        playStork = new Texture("artwork/storkmenu.png");
 
-        playStork = new Texture("storkmenu.png");
-        playBtn = new Texture("play.png");
+    }
 
-        font.getData().setScale(0.3f);
-        font.setColor(0,0,1,0);
+    @Override
+    public void pause() {
 
-        font.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
     }
 
     @Override
     public void dispose() {
         background.dispose();
         playStork.dispose();
-        playBtn.dispose();
-        font.dispose();
     }
 
 
     @Override
     public void handleInput() {
         if(Gdx.input.justTouched() ) {
-            gsm.set(new PlayState(gsm,cam,viewport,stage));
-            dispose();
+            Vector3 v = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+            v = cam.unproject(v);
+//            Gdx.app.debug("Input: ",v.x + " , " + v.y);
+
+            if(v.y < 20){
+                if(v.x > 10 && v.x < 20 ) {
+
+                }
+
+                else if(v.x > 30 && v.x < 40 ){
+
+                }
+
+                else if(v.x > 45 && v.x < 55 ){
+                    gsm.set(new PlayState(gsm, cam, viewport, stage));
+                    dispose();
+                }
+
+                else if(v.x > 65 && v.x < 70 ){
+
+                }
+
+                else if(v.x > 80 && v.x < 90 ){
+
+                }
+            }
         }
     }
 
     @Override
     public void update(float dt) {
         handleInput();
-
     }
 
     @Override
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
+
         sb.begin();
-        sb.draw(background,0,0, StorkLightGameClass.WIDTH,StorkLightGameClass.HEIGHT);
-
-        sb.draw(playStork,10,10,80,80);
-        sb.draw(playBtn,(cam.viewportWidth/2) - (playBtn.getWidth()/2),(cam.viewportHeight/2) - (playBtn.getHeight()/2) - 150);
-
-//        font.draw(sb, "Stork Light", 50, 50);
+        sb.draw(background,0,0, 100,100);
+        sb.draw(playStork,10 ,20 ,80,80);
+        font.draw(sb,"Press Play to Start", 30,30);
         sb.end();
+
     }
 }
