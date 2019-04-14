b102c0595bb13460e92aeaffed718319e4100a1b
Sanjeev Tripathi <sanjeevmail309@gmail.com>
2019-04-15 01:01:50 +0530

    feat(pause, timeKeeping): Added pause functionality on game screen, Added timeKeeping to display game time

--- core/src/com/studio/sanjeev/storklight/states/PlayState.java
+++ core/src/com/studio/sanjeev/storklight/states/PlayState.java
@@ -5,11 +5,13 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.studio.sanjeev.storklight.elements.CollectibleOrbs;
 import com.studio.sanjeev.storklight.sprites.Stork;
+import com.studio.sanjeev.storklight.utility.Prefs;
 
 /**
  * Created by sanjeev309 on 3/18/18.
@@ -20,26 +22,32 @@ public class PlayState extends State {
     private CollectibleOrbs orbs;
     Array<Texture> textures = new Array<Texture>();
     private BitmapFont font;
-    private Texture lifeTex;
+    private Texture lifeTex, pauseTex;
     private int score = 0;
     private int lives = 5;
+    private long startTime;
+    public Prefs prefs;
+
+    private static boolean PAUSED = false;
 
     public PlayState(GameStateManager gsm, OrthographicCamera cam, Viewport viewport,Stage  stage) {
         super(gsm, cam, viewport, stage);
 
+        prefs = new Prefs();
         stork = new Stork(0, 50);
-        lifeTex = new Texture(Gdx.files.internal("lifes.png"));
+        lifeTex = new Texture(Gdx.files.internal("artwork/lifes.png"));
+        pauseTex = new Texture(Gdx.files.internal("artwork/pause.png"));
         orbs = new CollectibleOrbs(cam);
         font = new BitmapFont(Gdx.files.internal("fonts/abel.fnt"), Gdx.files.internal("fonts/abel.png"), false);
         font.getData().setScale(0.15f);
-        textures.add(new Texture("n0.png"));
+        textures.add(new Texture("artwork/night_bg.png"));
         textures.get(textures.size - 1).setWrap(Texture.TextureWrap.ClampToEdge, Texture.TextureWrap.ClampToEdge);
 
         for (int i = 1; i <= 4; i++) {
-            textures.add(new Texture(i + ".png"));
+            textures.add(new Texture("artwork/"+i + ".png"));
             textures.get(textures.size - 1).setWrap(Texture.TextureWrap.MirroredRepeat, Texture.TextureWrap.MirroredRepeat);
         }
-        Gdx.app.debug("Stage:", stage.getActors().size + "");
+//        Gdx.app.debug("Stage:", stage.getActors().size + "");
 
         if (stage.getActors().size == 0) {
             ParallaxBackground parallaxBackground = new ParallaxBackground(textures, cam);
@@ -47,22 +55,49 @@ public class PlayState extends State {
             parallaxBackground.setSpeed(1);
             stage.addActor(parallaxBackground);
         }
+
+        startTime = System.currentTimeMillis();
     }
 
     @Override
     protected void handleInput() {
-        if(Gdx.input.isTouched()){
-            stork.fly();
+
+
+        if(Gdx.input.isTouched() ) {
+
+            Vector3 v = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+            v = cam.unproject(v);
+
+            if (!PAUSED) {
+                stork.fly();
             }
+
+            else {
+                PAUSED = false;
+            }
+
+            if (v.y > 90 && v.x > 90) {
+                PAUSED = true;
+            }
+        }
+
+
+        else
+        {
+            stork.descend();
+        }
+
     }
 
     @Override
     public void update(float dt) {
-        cam.update();
         handleInput();
-        stork.update(dt);
-        orbs.update(dt);
-        CollisionCheckMain();
+        if (!PAUSED) {
+            cam.update();
+            stork.update(dt);
+            orbs.update(dt);
+            CollisionCheckMain();
+        }
     }
 
     @Override
@@ -77,7 +112,7 @@ public class PlayState extends State {
         sb.end();
 
         sb.begin();
-        font.draw(sb," S c o r e : "+ getScore(), 55 , 95);
+        font.draw(sb,"Score: "+ getScore(), 40 , 95);
         sb.end();
 
         sb.begin();
@@ -85,27 +120,52 @@ public class PlayState extends State {
             sb.draw(lifeTex, 4*i - 4 + 2 , 100 - 10,4,6);
 
         }
+
+        sb.draw(pauseTex,92,90,5,5);
+
         sb.end();
+
+        if (PAUSED){
+
+            sb.begin();
+            font.draw(sb,"Touch Anywhere to Continue", 30,50);
+            sb.end();
         }
+        else{
+            sb.begin();
+            font.draw(sb," | Time: "+ getPlayTime(), 58 , 95);
+            sb.end();
+        }
+    }
+
+    @Override
+    public void pause() {
+        PAUSED = true;
+
+    }
 
     @Override
     public void dispose() {
         orbs.dispose();
         font.dispose();
+        stork.dispose();
+
     }
 
     public void CollisionCheckMain(){
         int temp;
         temp = orbs.checkCollision(stork.getBoundingRectangle());
-//        Gdx.app.debug("collision",temp + ":" + stork.getBoundingRectangle());
+
         if(temp==1){
+            stork.reward();
             score +=temp;
-            if (score%20==0){
+            if (score%20==0 && lives <=9){
                 lives+=1;
             }
         }
         else
             if(temp == -1){
+                stork.punish();
                 lives -=1;
         }
         checkKillCondition();
@@ -119,8 +179,21 @@ public class PlayState extends State {
         }
     }
 
-    public int getScore(){
+    private int getScore(){
         return score;
+    }
+
+    private String getPlayTime(){
+        long hours=0, minutes=0, seconds=0, units;
+        String timeString;
+        units = (System.currentTimeMillis() - startTime)/1000 ;
+
+        hours = units / 3600;
+        minutes = (units % 3600) / 60;
+        seconds = units % 60;
+
+        timeString = String.format("%02d:%02d:%02d", hours, minutes, seconds);
+        return timeString;
     }
 
 
